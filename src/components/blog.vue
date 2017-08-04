9461b154dfd8c9bc0f280fc230133b7cf0682106
donghaitao <donghaitao@qq.com>
2017-08-04 16:11:49 +0800

    app init

---
+++ src/components/blog.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="blog-container fadeInRight animated">
+		博客
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.blog-container {
+		height: 100%;
+		background: #fff;
+		opacity: 0;
+	}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="index-container fadeInRight animated">
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js Apps'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+	.index-container {
+	    height: 100%;
+	    background: #fff;
+	    opacity: 0;
+	  }
+</style>
